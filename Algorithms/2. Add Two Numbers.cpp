4bec8afca41b77cb8d298cb3e8278f3d84515772
Korepanov Grigorii Alekseevich <d33fur@gmail.com>
2023-10-22 15:06:07 +0300

    Create 2. Add Two Numbers.cpp

---
+++ Algorithms/2. Add Two Numbers.cpp
@@ -0,0 +1,45 @@
+/*
+2. Add Two Numbers
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* tail = head;
+        int nn = 0;
+
+        while (l1 != nullptr || l2 != nullptr || nn != 0) {
+            int n1 = (l1 != nullptr) ? l1->val : 0;
+            int n2 = (l2 != nullptr) ? l2->val : 0;
+
+            int sum = n1 + n2 + nn, digit = sum % 10;
+            nn = sum / 10;
+
+            ListNode* newNode = new ListNode(digit);
+            tail->next = newNode;
+            tail = tail->next;
+
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        }
+
+        ListNode* result = head->next;
+        delete head;
+        return result;
+    }
+};
